a0b2dc87e1c0c52e6c7b456bebc0a9098b069a5e
Fabiano Muscardi <sr.fabiano.muscardi@gmail.com>
2022-04-09 20:20:56 -0300

    Created project

---
+++ src/main/java/com/example/fabianomuscardi/workshopmongo/ProjetoApiRestfulComSprintBootEBancoMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.fabianomuscardi.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoApiRestfulComSprintBootEBancoMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoApiRestfulComSprintBootEBancoMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fabianomuscardi/workshopmongo/ProjetoApiRestfulComSprintBootEBancoMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fabianomuscardi.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoApiRestfulComSprintBootEBancoMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
